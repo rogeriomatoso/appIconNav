1cccb9204dd328b4f672d3ff1c5e94a9713094e2
Rogerio Matoso <rogeriomatoso@yahoo.com.br>
2021-05-20 22:31:23 -0300

    Adicionado Drawer

--- App.js
+++ App.js
@@ -1,62 +1,21 @@
 import React from 'react';
 import { NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {createStackNavigator} from '@react-navigation/stack';
-import Icon from 'react-native-vector-icons/Ionicons';
+import {createDrawerNavigator} from '@react-navigation/drawer'; 
 
 import Home from './src/pages/Home';
 import Sobre from './src/pages/Sobre';
 import Informacao from './src/pages/Informacao';
 
-const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
-
-const icons = {
-  Home:{
-    name: 'ios-home'
-  },
-  Sobre:{
-    name: 'ios-people'
-  },
-  Informacao:{
-    name: 'ios-call'
-  }
-}
-
-function Tabs() {
-  return (    
-      <Tab.Navigator
-        screenOptions={({route})=>({
-          tabBarIcon:({color, size})=> {
-            const {name} = icons[route.name];
-            return <Icon name={name} color={color} size={size}/>
-          }
-        })}
-
-        //personalização da tabBar
-
-        tabBarOptions={{
-          style:{
-           backgroundColor: '#000',
-           activeTintColor: '#FFF',  
-           //inactiveTintColor: '#FF0000'          
-          }
-        }}
-      >
-        <Tab.Screen name="Home" component={Home}/>
-        <Tab.Screen name="Sobre" component={Sobre}/>        
-      </Tab.Navigator>
-    
-  );
-}
+const Drawer = createDrawerNavigator();
 
 export default function App(){
   return(
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component ={Tabs} options = {{headerShown: false}}/>
-        <Stack.Screen name='Informacao' component ={Informacao}/>
-      </Stack.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name='Home' component={Home}/>
+        <Drawer.Screen name='Sobre' component={Sobre}/>
+        <Drawer.Screen name='Informacao' component={Informacao}/>
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
